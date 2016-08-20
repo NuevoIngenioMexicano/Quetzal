8f92f6ea451bd1fd3eb381f8aa345fdfbd969c66
Eduardo <eduardoa@revartsgaming.com>
2016-08-20 15:08:33 -0500

    Test script

---
+++ Quetzal/Quetzal/Assets/BtnTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Print(int i){
+		Debug.Log (i);
+	}
+}
